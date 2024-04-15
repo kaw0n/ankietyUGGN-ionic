4d94e32ccf3240001fb669bc329edee814eb0a16
kaw0n <130693205+kaw0n@users.noreply.github.com>
2024-04-15 08:05:07 +0200

    przed zajeciami 15.04

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  surveyCode: number=100000;
+
+  surveyData: SurveyData={
+    University: "",
+    Major: "",
+    Sem:"",
+    Lecturer:"",
+    Subject:"",
+    LectureType:"",
+    Group:""
+  }
 
   constructor() {}
+
+  getSurveyData(){
+    //pobranie z backendu
+
+    //przypisanie do modelu
+    this.surveyData = {
+      University:"University of Gdańsk",
+      Major:"AiB",
+      Sem:"2024 letni",
+      Lecturer: "Michał Kuciapski",
+      Subject:"Technologie e-lerningowe 2.0",
+      LectureType:"Lab",
+      Group:"S22-31"
+
+
+    }
+
+  }
+}
+
+interface SurveyData{
+  University : string
+  Major : string
+  Sem : string
+  Lecturer: string
+  Subject: string
+  LectureType: string
+  Group: string
 
 }
